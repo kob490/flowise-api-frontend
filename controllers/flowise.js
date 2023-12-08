49dd535805336a63597b7d498c905d40200b690e
Koby O <36440344+kob490@users.noreply.github.com>
2023-12-08 14:35:47 -0700

    Update flowise.js

--- controllers/flowise.js
+++ controllers/flowise.js
@@ -8,7 +8,22 @@ export const createPrediction = async (req, res) => {
       question: message,
     };
 
-    res.status(200).json({ message: "Demo Response" });
+    const response = await fetch(
+      `${process.env.FLOWISE_URL}/api/v1/prediction/${process.env.FLOW_ID}`,
+      {
+        body: JSON.stringify(flowiseData),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.FLOWISE_API_KEY}`,
+        },
+        method: "POST",
+      }
+    );
+
+    const data = await response.json();
+    console.log("data", data);
+
+    res.status(200).json({ message: data.text });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
